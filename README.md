3fc56da6cf7dd80914bd28ad1ae793fff8022f6d
HuJohner <hujohner@gmail.com>
2023-02-11 09:02:17 +0000

    Initial commit

--- README.md
+++ README.md
@@ -6,164 +6,61 @@
 </p>
 
 
-# Homebridge Platform Plugin Template
+# Homebridge Apple TV
 
-This is a template Homebridge platform plugin and can be used as a base to help you get started developing your own plugin.
+This is a Homebridge plugin that exposes Apple TVs to HomeKit, noteably the power state as a switch and optionally the device state (playing, paused, etc.) as motion sensors.
 
-This template should be used in conjunction with the [developer documentation](https://developers.homebridge.io/). A full list of all supported service types, and their characteristics is available on this site.
+## Installation
 
-## Clone As Template
-
-Click the link below to create a new GitHub Repository using this template, or click the *Use This Template* button above.
-
-<span align="center">
-
-### [Create New Repository From Template](https://github.com/homebridge/homebridge-plugin-template/generate)
-
-</span>
-
-## Setup Development Environment
-
-To develop Homebridge plugins you must have Node.js 12 or later installed, and a modern code editor such as [VS Code](https://code.visualstudio.com/). This plugin template uses [TypeScript](https://www.typescriptlang.org/) to make development easier and comes with pre-configured settings for [VS Code](https://code.visualstudio.com/) and ESLint. If you are using VS Code install these extensions:
-
-* [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
-
-## Install Development Dependencies
-
-Using a terminal, navigate to the project folder and run this command to install the development dependencies:
-
+To make this plugin work you need to install [PyATV](https://pypi.org/project/pyatv/). To ensure you did it right run
 ```
-npm install
+atvremote --version
 ```
 
-## Update package.json
+## Configuration
+Configuration can be done using [Homebridge Config UI X](https://github.com/oznu/homebridge-config-ui-x).
 
-Open the [`package.json`](./package.json) and change the following attributes:
+## Sample Configuration
 
-* `name` - this should be prefixed with `homebridge-` or `@username/homebridge-` and contain no spaces or special characters apart from a dashes
-* `displayName` - this is the "nice" name displayed in the Homebridge UI
-* `repository.url` - Link to your GitHub repo
-* `bugs.url` - Link to your GitHub repo issues page
-
-When you are ready to publish the plugin you should set `private` to false, or remove the attribute entirely.
-
-## Update Plugin Defaults
-
-Open the [`src/settings.ts`](./src/settings.ts) file and change the default values:
-
-* `PLATFORM_NAME` - Set this to be the name of your platform. This is the name of the platform that users will use to register the plugin in the Homebridge `config.json`.
-* `PLUGIN_NAME` - Set this to be the same name you set in the [`package.json`](./package.json) file. 
-
-Open the [`config.schema.json`](./config.schema.json) file and change the following attribute:
-
-* `pluginAlias` - set this to match the `PLATFORM_NAME` you defined in the previous step.
-
-## Build Plugin
-
-TypeScript needs to be compiled into JavaScript before it can run. The following command will compile the contents of your [`src`](./src) directory and put the resulting code into the `dist` folder.
-
-```
-npm run build
-```
-
-## Link To Homebridge
-
-Run this command so your global install of Homebridge can discover the plugin in your development environment:
-
-```
-npm link
-```
-
-You can now start Homebridge, use the `-D` flag so you can see debug log messages in your plugin:
-
-```
-homebridge -D
-```
-
-## Watch For Changes and Build Automatically
-
-If you want to have your code compile automatically as you make changes, and restart Homebridge automatically between changes, you first need to add your plugin as a platform in `~/.homebridge/config.json`:
-```
+```yaml
 {
-...
-    "platforms": [
+    "platform": "AppleTV",
+    "name": "Apple TV",
+    "devices": [
         {
-            "name": "Config",
-            "port": 8581,
-            "platform": "config"
-        },
-        {
-            "name": "<PLUGIN_NAME>",
-            //... any other options, as listed in config.schema.json ...
-            "platform": "<PLATFORM_NAME>"
+            "name": "Living Room",
+            "host": "192.168.0.36",
+            "credentials": "...",
+            "device_state_sensors": [
+                "idle",
+                "paused",
+                "playing"
+            ]
         }
     ]
 }
 ```
+### Configuration Definition
 
-and then you can run:
+* **platform**: The identifier for the platform (*AppleTv*).
+* **devices**: A list of devices you would like to register with the platform.     
+  * **name**: The name of the Apple TV.    
+  * **host**: The IP of the device.
+  * **credentials**: The credentials needed to authorise connection to the device.
+  * **device_state_sensors** [*optional*]: Enables a "motion sensor" for detecting device states. Possible values: "idle", "loading", "paused", "playing", "seeking", "stopped"
 
-```
-npm run watch
-```
+## Retrieving credentials
 
-This will launch an instance of Homebridge in debug mode which will restart every time you make a change to the source code. It will load the config stored in the default location under `~/.homebridge`. You may need to stop other running instances of Homebridge while using this command to prevent conflicts. You can adjust the Homebridge startup command in the [`nodemon.json`](./nodemon.json) file.
+In order to retrieve credentials for your Apple TV, please follow these step
 
-## Customise Plugin
-
-You can now start customising the plugin template to suit your requirements.
-
-* [`src/platform.ts`](./src/platform.ts) - this is where your device setup and discovery should go.
-* [`src/platformAccessory.ts`](./src/platformAccessory.ts) - this is where your accessory control logic should go, you can rename or create multiple instances of this file for each accessory type you need to implement as part of your platform plugin. You can refer to the [developer documentation](https://developers.homebridge.io/) to see what characteristics you need to implement for each service type.
-* [`config.schema.json`](./config.schema.json) - update the config schema to match the config you expect from the user. See the [Plugin Config Schema Documentation](https://developers.homebridge.io/#/config-schema).
-
-## Versioning Your Plugin
-
-Given a version number `MAJOR`.`MINOR`.`PATCH`, such as `1.4.3`, increment the:
-
-1. **MAJOR** version when you make breaking changes to your plugin,
-2. **MINOR** version when you add functionality in a backwards compatible manner, and
-3. **PATCH** version when you make backwards compatible bug fixes.
-
-You can use the `npm version` command to help you with this:
-
-```bash
-# major update / breaking changes
-npm version major
-
-# minor update / new features
-npm version update
-
-# patch / bugfixes
-npm version patch
-```
-
-## Publish Package
-
-When you are ready to publish your plugin to [npm](https://www.npmjs.com/), make sure you have removed the `private` attribute from the [`package.json`](./package.json) file then run:
+1. Execute the following command to scan for Apple TVs:
 
 ```
-npm publish
+atvremote scan
 ```
-
-If you are publishing a scoped plugin, i.e. `@username/homebridge-xxx` you will need to add `--access=public` to command the first time you publish.
-
-#### Publishing Beta Versions
-
-You can publish *beta* versions of your plugin for other users to test before you release it to everyone.
-
-```bash
-# create a new pre-release version (eg. 2.1.0-beta.1)
-npm version prepatch --preid beta
-
-# publish to @beta
-npm publish --tag=beta
-```
-
-Users can then install the  *beta* version by appending `@beta` to the install command, for example:
+2. Choose the device with which you would like to pair and run the following command:
 
 ```
-sudo npm install -g homebridge-example-plugin@beta
+atvremote -s {YOUR-APPLE-ID-IP-ADDRESS} --protocol companion pair
 ```
-
-
+3. Enter the PIN shown on your device.
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,141 +1,117 @@
+import pyatv, { NodePyATVDevice, NodePyATVDeviceEvent, NodePyATVDeviceState, NodePyATVPowerState } from '@sebbo2002/node-pyatv';
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { AppleTVPlatform } from './platform';
 
 /**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
+ * AppleTV Accessory
  */
-export class ExamplePlatformAccessory {
-  private service: Service;
+export class AppleTVAccessory {
+  static IDLE_SERVICE = 'Idle Service';
+  static LOADING_SERVICE = 'Loading Service';
+  static PAUSED_SERVICE = 'Paused Service';
+  static PLAYING_SERVICE = 'Playing Service';
+  static SEEKING_SERVICE = 'Seeking Service';
+  static STOPPED_SERVICE = 'Stopped Service';
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
+  private atv: NodePyATVDevice;
+  private accessoryInfoService: Service;
+  private powerStateService: Service;
+  private idleService: Service | undefined;
+  private loadingService: Service | undefined;
+  private pausedService: Service | undefined;
+  private playingService: Service | undefined;
+  private seekingService: Service | undefined;
+  private stoppedService: Service | undefined;
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: AppleTVPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
+    this.atv = pyatv.device({
+      name: this.accessory.context.device.name,
+      host: this.accessory.context.device.host,
+      airplayCredentials: this.accessory.context.device.credentials,
+    });
 
-    // set accessory information
-    this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+    this.accessoryInfoService = this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Apple Inc.')
+      .setCharacteristic(this.platform.Characteristic.Model, 'AppleTV')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, this.atv.id ?? 'Serial');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    this.powerStateService = this.accessory.getService(this.platform.Service.Switch)
+      || this.accessory.addService(this.platform.Service.Switch);
+    this.powerStateService.setCharacteristic(this.platform.Characteristic.Name, 'Power State');
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    if (this.accessory.context.device.device_state_sensors?.includes('idle')) {
+      this.idleService = this.accessory.getService(AppleTVAccessory.IDLE_SERVICE)
+        || this.accessory.addService(this.platform.Service.MotionSensor, AppleTVAccessory.IDLE_SERVICE, 'idle');
+      this.idleService.setCharacteristic(this.platform.Characteristic.Name, 'Idle');
+    }
+    if (this.accessory.context.device.device_state_sensors?.includes('loading')) {
+      this.loadingService = this.accessory.getService(AppleTVAccessory.LOADING_SERVICE)
+        || this.accessory.addService(this.platform.Service.MotionSensor, AppleTVAccessory.LOADING_SERVICE, 'loading');
+      this.loadingService.setCharacteristic(this.platform.Characteristic.Name, 'Loading');
+    }
+    if (this.accessory.context.device.device_state_sensors?.includes('paused')) {
+      this.pausedService = this.accessory.getService(AppleTVAccessory.PAUSED_SERVICE)
+        || this.accessory.addService(this.platform.Service.MotionSensor, AppleTVAccessory.PAUSED_SERVICE, 'paused');
+      this.pausedService.setCharacteristic(this.platform.Characteristic.Name, 'Paused');
+    }
+    if (this.accessory.context.device.device_state_sensors?.includes('playing')) {
+      this.playingService = this.accessory.getService(AppleTVAccessory.PLAYING_SERVICE)
+        || this.accessory.addService(this.platform.Service.MotionSensor, AppleTVAccessory.PLAYING_SERVICE, 'playing');
+      this.playingService.setCharacteristic(this.platform.Characteristic.Name, 'Playing');
+    }
+    if (this.accessory.context.device.device_state_sensors?.includes('seeking')) {
+      this.seekingService = this.accessory.getService(AppleTVAccessory.SEEKING_SERVICE)
+        || this.accessory.addService(this.platform.Service.MotionSensor, AppleTVAccessory.SEEKING_SERVICE, 'seeking');
+      this.seekingService.setCharacteristic(this.platform.Characteristic.Name, 'Seeking');
+    }
+    if (this.accessory.context.device.device_state_sensors?.includes('stopped')) {
+      this.stoppedService = this.accessory.getService(AppleTVAccessory.STOPPED_SERVICE)
+        || this.accessory.addService(this.platform.Service.MotionSensor, AppleTVAccessory.STOPPED_SERVICE, 'stopped');
+      this.stoppedService.setCharacteristic(this.platform.Characteristic.Name, 'Stopped');
+    }
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+    for (const service of this.accessory.services) {
+      if (service === this.accessoryInfoService || service === this.powerStateService || service === this.idleService
+        || service === this.loadingService || service === this.pausedService || service === this.playingService
+        || service === this.seekingService || service === this.stoppedService) {
+        continue;
+      }
+      this.platform.log.info('Removing unused service: ', service.name);
+      this.accessory.removeService(service);
+    }
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
-
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
-
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+    this.powerStateService.getCharacteristic(this.platform.Characteristic.On)
+      .onSet(this.setOn.bind(this));
+    this.atv.on('update:powerState', (event: NodePyATVDeviceEvent | Error) => {
+      if (event instanceof Error) {
+        return;
+      }
+      this.powerStateService.getCharacteristic(this.platform.Characteristic.On).updateValue(event.newValue === NodePyATVPowerState.on);
+    });
+    this.atv.on('update:deviceState', (event: NodePyATVDeviceEvent | Error) => {
+      if (event instanceof Error) {
+        return;
+      }
+      this.idleService?.setCharacteristic(this.platform.Characteristic.MotionDetected, event.newValue === NodePyATVDeviceState.idle);
+      this.loadingService?.setCharacteristic(this.platform.Characteristic.MotionDetected, event.newValue === NodePyATVDeviceState.loading);
+      this.pausedService?.setCharacteristic(this.platform.Characteristic.MotionDetected, event.newValue === NodePyATVDeviceState.paused);
+      this.playingService?.setCharacteristic(this.platform.Characteristic.MotionDetected, event.newValue === NodePyATVDeviceState.playing);
+      this.seekingService?.setCharacteristic(this.platform.Characteristic.MotionDetected, event.newValue === NodePyATVDeviceState.seeking);
+      this.stoppedService?.setCharacteristic(this.platform.Characteristic.MotionDetected, event.newValue === NodePyATVDeviceState.stopped);
+    });
   }
 
   /**
    * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
    */
   async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+    value ? await this.atv.turnOn() : await this.atv.turnOff();
 
     this.platform.log.debug('Set Characteristic On ->', value);
-  }
-
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
-
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn;
-  }
-
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
   }
 
 }
